369321acb28f3d27148b83e2f8e7318b40f98585
Premjith <premjithprasad226@gmail.com>
2026-03-27 11:25:44 +0530

    commit

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -12,6 +12,13 @@ urlpatterns = [
     path('user_view/',views.user_view,name='user_view'),
     path('add_category/',views.add_category,name='add_category'),
     path('category_list/',views.category_list,name='category_list'),
-    path('delete_category/<int:id>/',views.delete_category,name='delete_category')
+    path('delete_category/<int:id>/',views.delete_category,name='delete_category'),
+    path('update_category/<int:id>/',views.update_category,name='update_category'),
+    path('deactivate_category/<int:id>/',views.deactivate_category,name='deactivate_category'),
+    path('add_subcategory/',views.add_subcategory,name='add_subcategory'),
+    path('subcategory_list/',views.subcategory_list,name='subcategory_list'),
+    path('activate_category/<int:id>/',views.active_category,name='activate_category'),
+    path('update_subcategory/<int:id>/',views.update_subcategory,name='update_subcategory'),
+    path('delete_subcategory/<int:id>/',views.delete_subcategory,name='delete_subcategory'),
 
 ]
