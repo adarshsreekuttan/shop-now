e97f469a59c1b9663acb61695a8db118e087a076
Adarsh <adarsh.rohini11@gmail.com>
2026-04-01 12:46:05 +0530

    j:

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+
+def customer_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            if user.role != 'customer':
+                return render(request, 'customer/login.html',
+                              {'error': "Seller cannot login here"})
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'customer/login.html',
+                          {"error": "Invalid email or password"})
+
+    return render(request, 'customer/login.html')
